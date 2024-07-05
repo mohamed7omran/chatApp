253d7b9bbe315c8a2f5ce0dc93955b443eab4685
mohamed7omran <mo1234512345ed@gmail.com>
2024-07-05 21:44:46 +0300

    api

--- api/index.js
+++ api/index.js
@@ -1,15 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
+const jwt = require("jsonwebtoken");
+const User = require("./models/user");
 
 dotenv.config();
+mongoose.connect(process.env.MONGO_URL);
+const jwtSecret = process.env.JWT_SECRET;
 
 app.get("/test", (req, res) => {
   res.json("test ok ");
 });
-
-app.post("/register", (req, res) => {});
+//! new
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const createdUser = await User.create({ username, password });
+  jwt.sign({ userId: createdUser, id }, jwtSecret, (err, token) => {
+    if (err) throw err;
+    res.cookie("token", token).status(201).json("ok");
+  });
+});
 
 app.listen(4000, () => {
-  console.log("i am listen");
+  console.log("i am listen 4000");
 });
