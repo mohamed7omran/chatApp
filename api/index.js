25d9811624f2889e0a9843d23d8f4d616789a77b
mohamed7omran <mo1234512345ed@gmail.com>
2024-07-06 18:50:20 +0300

    updated

--- api/index.js
+++ api/index.js
@@ -5,48 +5,65 @@ const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const User = require("./models/user");
 
-// ! new
 dotenv.config();
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("connecting is successful");
-  })
-  .catch(() => {
-    console.log("error connecting to Mongoose");
-  });
-const jwtSecret = process.env.JWT_SECRET;
+mongoose.connect(process.env.MONGO_URL);
+mongoose.connection.on("connected", () => {
+  console.log("connected to database");
+});
+mongoose.connection.on("error", (err) => {
+  console.error(`failed to connected to database : ${err}`);
+});
+// const jwtSecret = process.env.JWT_SECRET;
 
 const app = express();
 app.use(express.json());
-// !new
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL,
-  })
-);
 
-app.get("/test", (req, res) => {
-  res.json("test ok ");
+// !new
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: process.env.CLIENT_URL,
+//   })
+// );
+
+app.get("/register", async (req, res) => {
+  const user = await User.find();
+  res.json(user);
 });
-//! new
+
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
   try {
-    console.log("username");
-    const createdUser = await User.create({ username, password });
-    jwt.sign({ userId: createdUser, id }, jwtSecret, {}, (err, token) => {
-      if (err) throw err;
-      res.cookie("token", token).status(201).json({
-        id: createdUser._id,
-      });
-    });
+    const createdUser = await User.create({ username, password }).then((user) =>
+      res.status(200).json({
+        message: "User successfully created",
+        user,
+      })
+    );
   } catch (err) {
-    if (err) throw err;
-    res.status(500).json("error");
+    res.status(401).json({
+      message: "User not successful created",
+    });
   }
+  // createdUser.save();
 });
+//! new
+// app.post("/register", async (req, res) => {
+//   const { username, password } = req.body;
+//   try {
+//     console.log("username");
+//     const createdUser = await User.create({ username, password });
+//     jwt.sign({ userId: createdUser, id }, jwtSecret, {}, (err, token) => {
+//       if (err) throw err;
+//       res.cookie("token", token).status(201).json({
+//         id: createdUser._id,
+//       });
+//     });
+//   } catch (err) {
+//     if (err) throw err;
+//     res.status(500).json("error");
+//   }
+// });
 
 app.listen(8000, () => {
   console.log("i'am listen ,port 8000");
